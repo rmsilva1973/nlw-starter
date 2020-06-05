f3bebdc509e5fd14ea0a092e2d186adf3735d885
Renato Moutinho Silva <renato.moutinho@gmail.com>
2020-06-05 17:34:48 -0300

    Almost done...

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const server = express()
 
+const db = require("./database/db")
+
 // Configurar pasta pública
 server.use(express.static("public"))
+server.use(express.urlencoded({extended: true}))
 
 // Utilizando template engine
 const nunjucks = require("nunjucks")
@@ -20,8 +23,18 @@ server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+    console.log(req.body)
+    return res.send('ok')
+})
+
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+    db.all("SELECT * FROM places", function(err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+        return res.render("search-results.html", {places: rows})
+    })
 })
 
 // Ligar o servidor
